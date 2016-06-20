cd361191f8720de9b883d62180e820391e9b75b4
Mastere 1 IT <mastere1it@spotlight.sto-002.saint-augustin.in.iesanetwork.com>
2016-06-20 16:09:30 +0200

    #4 setter getter done

--- iesaPlayground.playground/Contents.swift
+++ iesaPlayground.playground/Contents.swift
@@ -3,29 +3,68 @@
 import UIKit
 
 class Shape {
-
-    // var sidesCount = 0
     
     // Exemple de propriétés
-    var sides: Int
-    var nom: String
+    var name: String
     
     // Fonction d'initialisation
-    init(sides: Int, nom: String) {
-        self.sides = sides
-        self.nom = nom
+    init(name: String) {
+        self.name = name
     }
     
-    func getInfo() -> String {
-        return "A shape with \(sides) sides."
-    }
     
 }
 
-var triangle = Shape(sides:3, nom: :"triangle")
-print( triangle.getInfo())
-print( triangle.sides)
-print( triangle.nom)
+class Square : Shape{
+    var sidelength : Double
+    init( length : Double, name : String){
+        self.sidelength = length
+        super.init(name: name)
+        // super.nom = name
+        // super.sides = sides
+    }
+}
 
-triangle.sides = 5
-print( triangle.sides)
+var carre = Shape( name: "carre")
+print( carre.name)
+
+
+class NamedShaped {
+    var sidesCount : Int = 0
+    var name: String
+    init(name:String) { self.name = name }
+    func getInfo() -> String {
+        return "Nombre de cote =\(sidesCount)"
+    }
+    
+    
+}
+
+class EquilateralTriangle: NamedShaped {
+    var sidelength: Double = 0.0
+    init(length: Double, name: String) {
+        self.sidelength = length
+        super.init(name: name)
+        sidesCount = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sidelength
+        }
+        set {
+            sidelength = newValue / 3.0
+        }
+    }
+    
+    override func getInfo() -> String {
+        return "Length=\(sidelength)"
+    }
+}
+
+var triangle = EquilateralTriangle(length: 3.3, name: "a triangle")
+print(triangle.perimeter)
+print(triangle.sidelength)
+triangle.perimeter = 6.0
+print(triangle.perimeter)
+print(triangle.sidelength)
